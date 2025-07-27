26c860cebaeb28ffbdf5527af3fa43355f37d83b
blitz <146631108+blitzboah@users.noreply.github.com>
2025-07-27 11:54:28 +0530

    update

--- git-pusher.sh
+++ git-pusher.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-cd "/home/blitz/Documents/Obsidian Vault/vault/"
-
+cd "/path/to/your/Obsidian Vault/[vault-with-git]"
 if [ -n "$(git status --porcelain)" ]; then
   git add .
   git commit -m "pushed on $(date)"
 fi
-
 git push origin main
