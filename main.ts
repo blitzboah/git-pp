b4a332c350ad7ef7b1fc80cd455155308fc22732
blitz <146631108+blitzboah@users.noreply.github.com>
2025-07-27 11:53:15 +0530

    changes

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ export default class GitScriptPlugin extends Plugin {
       id: "git-pull",
       name: "git pull",
       callback: () => {
-        const pull = spawn("sh", ["/home/blitz/Documents/Obsidian Vault/.obsidian/plugins/git-pp/git-puller.sh"]);
+        const pull = spawn("sh", ["/path/to/your/Obsidian Vault/.obsidian/plugins/git-pp/git-puller.sh"]);
         pull.stdout.on("data", data => console.log(`stdout (pull): ${data}`));
         pull.stderr.on("data", data => console.warn(`stderr (pull): ${data}`));
         pull.on("error", err => console.error(`Error (pull): ${err.message}`));
@@ -18,7 +18,7 @@ export default class GitScriptPlugin extends Plugin {
       id: "git-push",
       name: "git push",
       callback: () => {
-        const push = spawn("sh", ["/home/blitz/Documents/Obsidian Vault/.obsidian/plugins/git-pp/git-pusher.sh"]);
+        const push = spawn("sh", ["/path/to/your/Obsidian Vault/.obsidian/plugins/git-pp/git-pusher.sh"]);
         push.stdout.on("data", data => console.log(`stdout (push): ${data}`));
         push.stderr.on("data", data => console.warn(`stderr (push): ${data}`));
         push.on("error", err => console.error(`Error (push): ${err.message}`));
@@ -26,4 +26,3 @@ export default class GitScriptPlugin extends Plugin {
     });
   }
 }
-
